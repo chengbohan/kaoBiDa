b4ac46bb26379eaf7cf3c01b70008929a4acd0e1
博瀚程 <842880648@qq.com>
2020-06-29 14:22:12 +0800

    ssss

---
+++ KaoBiDa/KaoBiDa/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  KaoBiDa
+//
+//  Created by mac on 2020/6/29.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ KaoBiDa/KaoBiDa/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  KaoBiDa
+//
+//  Created by mac on 2020/6/29.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"FFFFF");
+    NSLog(@"EEEEE");
+    // Do any additional setup after loading the view.
+}
+
+
+@end
